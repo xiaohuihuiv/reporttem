8ab279aa2528995d849d1afe772bf7ca6025af0e
Ssshi27 <1275999095@qq.com>
2024-12-23 22:16:30 +0800

    服务器端

---
+++ server/server.c
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2024 LingYun<lingyun@email.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  server.c
+ *    Description:  This file is server main function
+ *                 
+ *        Version:  1.0.0(23/12/24)
+ *         Author:  LingYun <lingyun@email.com>
+ *      ChangeLog:  1, Release initial version on "23/12/24 22:15:45"
+ *                 
+ ********************************************************************************/
+
+
